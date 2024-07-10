da20134b6245cb49cd93381ef959bf90db6f8f8d
AmmarMKamel <ammarkamel9899@gmail.com>
2024-07-11 02:10:21 +0300

    fix: change date format

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require("body-parser");
 const cors = require('cors')
 const mongoose = require('mongoose');
+const moment = require('moment');
 require('dotenv').config()
 
 const User = require('./models/user.model');
@@ -37,18 +38,18 @@ app.post('/api/users/:id/exercises', async (req, res, next) => {
   const exercise = new Exercise({
     description: req.body.description,
     duration: req.body.duration,
-    date: req.body.date ? req.body.date : new Date().toDateString(),
+    date: req.body.date ? new Date(req.body.date) : new Date(),
     userId: user
   });
 
   const savedExercise = await exercise.save();
 
   const response = {
-    _id: savedExercise._id,
+    _id: user._id,
     username: user.username,
     description: savedExercise.description,
     duration: savedExercise.duration,
-    date: savedExercise.date
+    date: savedExercise.date.toDateString()
   };
 
   res.status(201).json(response);
@@ -60,23 +61,32 @@ app.get('/api/users/:id/logs', async (req, res, next) => {
   const fromDate = req.query.from || '';
   const toDate = req.query.to || '';
 
-  const formattedStartDate = fromDate ?? new Date(fromDate).toDateString();
-  const formattedEndDate = toDate ?? new Date(toDate).toDateString();
+  // Use moment to parse and set start/end of day for date range
+  const validStartDate = fromDate ? moment(fromDate).startOf('day').toDate() : null;
+  const validEndDate = toDate ? moment(toDate).endOf('day').toDate() : null;
 
   const query = {
     userId: user._id,
-    ...( formattedStartDate || formattedEndDate ? {
+    ...(validStartDate || validEndDate ? {
       date: {
-        $gte: formattedStartDate,
-        $lte: formattedEndDate
+        ...(validStartDate ? { $gte: validStartDate } : {}),
+        ...(validEndDate ? { $lte: validEndDate } : {})
       }
     } : {})
   };
 
-  const exercises = await Exercise.find(query).limit(limit).select(('-userId -_id'));
-  const count = exercises.length;
+  console.log(query);
 
-  res.status(200).json({ count, ...user._doc, log: exercises });
+  const exercises = await Exercise.find(query).limit(limit).select(('-userId -_id'));
+
+  const formattedExercises = exercises.map(exercise => ({
+    ...exercise.toObject(),
+    date: exercise.date.toDateString()
+  }));
+
+  const count = formattedExercises.length;
+
+  res.status(200).json({ count, ...user._doc, log: formattedExercises });
 })
 
 let listener;
